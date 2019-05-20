3ceafd528543699994795a43af1cd54cd53a308b
Beatriz Oliveira <beatrizo237@gmail.com>
2019-05-20 19:07:43 -0300

    Postagem

---
+++ WikiLearn/src/bd/daos/PostagemDAO.java
@@ -0,0 +1,171 @@
+package bd.daos;
+
+import bd.BD;
+import bd.core.MeuResultSet;
+import bd.dbos.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+public class PostagemDAO {
+
+	public void inserir(Postagem postagem) {
+
+		try {
+
+			String query;
+
+			query = "INSERT INTO Postagem(datta,encerrada,pergunta) Values(?,?,?)";
+			BD.COMANDO.prepareStatement(query);
+			//BD.COMANDO.setString(1, postagem.getDono().getNome());
+			BD.COMANDO.setTimestamp(1, new Timestamp(postagem.getTime()));
+			BD.COMANDO.setBoolean(2, postagem.isEncerrado());
+			BD.COMANDO.setString(3, postagem.getPergunta());
+			BD.COMANDO.executeUpdate();
+			BD.COMANDO.commit();
+
+		} catch (SQLException erro) {
+			System.err.println("Erro ao inserir comentário" + erro.getMessage());
+		}
+	}
+
+	public static List<Postagem> listarPostagens(Usuario usuario) {
+		List<Postagem> lista = new ArrayList<Postagem>();
+
+		MeuResultSet resultado = null;
+
+		try {
+
+			String query;
+
+			query = "SELECT * FROM Postagem WHERE apelido = ? order by datta desc; ";
+			BD.COMANDO.prepareStatement(query);
+			BD.COMANDO.setString(1, usuario.getNome());
+
+			resultado = (MeuResultSet) BD.COMANDO.executeQuery();
+
+			while (resultado.next()) {
+				Postagem objP = new Postagem();
+
+				objP.setIdPostagem(resultado.getInt("idPostagem"));
+				objP.setDono(usuario);
+				objP.setData(resultado.getTimestamp("datta"));
+				objP.setPergunta(resultado.getString("pergunta"));
+
+				lista.add(objP);
+			}
+
+		} catch (SQLException erro) {
+			System.err.println("Erro ao inserir comentário" + erro.getMessage());
+
+		}
+		return lista;
+
+	}
+
+	public Postagem consultaPostagem(int id) {
+		Postagem objP = null;
+
+		MeuResultSet resultado = null;
+
+		try {
+
+			String query;
+
+			query = "SELECT * FROM Postagem WHERE idPostagem = ?";
+			BD.COMANDO.prepareStatement(query);
+			BD.COMANDO.setInt(1, id);
+
+			resultado = (MeuResultSet) BD.COMANDO.executeQuery();
+
+			if (resultado.next()) {
+				Usuario objU = new Usuario();
+				objP = new Postagem();
+				objP.setIdPostagem(resultado.getInt("idPostagem"));
+				objP.setPergunta(resultado.getString("pergunta"));
+				objP.setData(resultado.getTimestamp("datta"));
+				objP.setEncerrado(resultado.getBoolean("encerrada"));
+				objU.setNome(resultado.getString("apelido"));
+				objP.setDono(objU);
+			}
+			BD.COMANDO.close();
+
+		} catch (SQLException ex) {
+			System.err.println("Erro ao inserir comentário" + ex.getMessage());
+
+		}
+
+		return objP;
+
+	}
+
+	/*
+	public static List<Postagem> listarPostagens() {
+		List<Postagem> lista = new ArrayList<Postagem>();
+
+		try {
+
+			Connection conexao1 = ConexaoDB.getConnection();
+			String Query = "SELECT * FROM Postagem order by datta desc; ";
+			PreparedStatement BD.COMANDO = conexao1.prepareStatement(Query);
+			// JOptionPane.showMessageDialog(null,"Dados inseridos com Ãªxido");
+			ResultSet resul = BD.COMANDO.executeQuery();
+
+			while (resul.next()) {
+				Postagem objP = new Postagem();
+
+				objP.setIdPostagem(resul.getInt("idPostagem"));
+				objP.setData(resul.getTimestamp("datta"));
+				objP.setPergunta(resul.getString("pergunta"));
+
+				lista.add(objP);
+			}
+			BD.COMANDO.close();
+
+			// conexao1.close();
+		} catch (SQLException ex) {
+			JOptionPane.showMessageDialog(null, "Erro" + ex.toString());
+
+		} finally {
+			ConexaoDB.closeConnection();
+		}
+		return lista;
+
+	}
+
+	
+	public static boolean encerrarPostagem(Postagem postagem) {
+
+		boolean result = false;
+
+		try {
+
+			Connection conexao1 = ConexaoDB.getConnection();
+			String Query = "UPDATE postagem set encerrada = ? where idPostagem = ?";
+			PreparedStatement BD.COMANDO = conexao1.prepareStatement(Query);
+			BD.COMANDO.setBoolean(1, postagem.isEncerrado());
+			BD.COMANDO.setInt(2, postagem.getIdPostagem());
+
+			// JOptionPane.showMessageDialog(null,"Dados inseridos com Ãªxido");
+			BD.COMANDO.execute();
+			result = true;
+			BD.COMANDO.close();
+			// conexao1.close();
+		} catch (SQLException ex) {
+			JOptionPane.showMessageDialog(null, "Erro" + ex.toString());
+
+		} finally {
+			ConexaoDB.closeConnection();
+		}
+
+		return result;
+	}
+	
+	*/
+
+}
---
+++ WikiLearn/src/bd/dbos/Postagem.java
@@ -0,0 +1,156 @@
+package bd.dbos;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author Bia
+ */
+public class Postagem {
+
+	private Usuario dono;
+	private String pergunta;
+	private List<Comentario> comentarios;
+	private Date data;
+	private long time;
+	private List<Tags> tags;
+	private boolean encerrado;
+	private int idPostagem;
+
+	// falso = aberto true = fechado
+	public Postagem() {
+		this.data = new Date();
+		this.time = System.currentTimeMillis();
+		this.encerrado = false;
+		tags = new ArrayList<Tags>();
+		comentarios = new ArrayList<Comentario>();
+
+		/*
+		 * Comentario c = new Comentario(); c.setComentario("text"); comentarios.add(c);
+		 */
+	}
+
+	public Usuario getDono() {
+		return dono;
+	}
+
+	/**
+	 * @param dono
+	 *            the dono to set
+	 */
+	public void setDono(Usuario dono) {
+		this.dono = dono;
+	}
+
+	/**
+	 * @return the pergunta
+	 */
+	public String getPergunta() {
+		return pergunta;
+	}
+
+	/**
+	 * @param pergunta
+	 *            the pergunta to set
+	 */
+	public void setPergunta(String pergunta) {
+		this.pergunta = pergunta;
+	}
+
+	/**
+	 * @return the visitas
+	 */
+	public List<Comentario> getComentarios() {
+		return comentarios;
+	}
+
+	/**
+	 * @param comentarios
+	 *            the comentarios to set
+	 */
+	public void setComentarios(List<Comentario> comentarios) {
+		this.comentarios = comentarios;
+	}
+
+	/**
+	 * @return the data
+	 */
+	public Date getData() {
+		return data;
+	}
+
+	/**
+	 * @return the tags
+	 */
+	public List<Tags> getTags() {
+		return tags;
+	}
+
+	public void setTags(List<Tags> tags) {
+		this.tags = tags;
+	}
+
+	/**
+	 * @return the idPostagem
+	 */
+	public int getIdPostagem() {
+		return idPostagem;
+	}
+
+	/**
+	 * @param idPostagem
+	 *            the idPostagem to set
+	 */
+	public void setIdPostagem(int idPostagem) {
+		this.idPostagem = idPostagem;
+	}
+
+	/**
+	 * @param data
+	 *            the data to set
+	 */
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	/**
+	 * @return the encerrado
+	 */
+	public boolean isEncerrado() {
+		return encerrado;
+	}
+
+	public void setEncerrado(boolean enc) {
+		this.encerrado = enc;
+	}
+
+	public String getRespostasFormatadas() {
+		StringBuilder sb = new StringBuilder();
+
+		DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+		for (Comentario c : comentarios) {
+			sb.append(c.getDono().getNome());
+			sb.append(" disse: \n");
+			sb.append(c.getComentario());
+			sb.append("\n");
+			sb.append(dateFormat.format(c.getData()));
+			sb.append("\n");
+
+		}
+		return sb.toString();
+
+	}
+
+	/**
+	 * @return the time
+	 */
+	public long getTime() {
+		return time;
+	}
+
+}
